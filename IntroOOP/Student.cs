2721295707f239ed177540c5031f1140c1f08542
Infarh <shmachilin@yandex.ru>
2022-06-23 22:02:02 +0300

    Добавлены студенты и группы студентов

---
+++ IntroOOP/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace IntroOOP;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Name { get; set; }
+
+    public string Patronymic { get; set; }
+}
---
+++ IntroOOP/StudentsGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroOOP;
+
+public class StudentsGroup
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public List<Student> Students { get; set; } = new();
+}
