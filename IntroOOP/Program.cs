f7c496d6dbd59dd9b4c901abd64f6171dca34cf9
Infarh <shmachilin@yandex.ru>
2022-06-27 20:15:04 +0300

    Поля, свойства, методы, машинки

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,4 +1,21 @@
-﻿
+﻿using IntroOOP.Transport;
 
-Console.WriteLine("Hello, World!");
+Car car1 = new Car();
+//car1.SetX(5);
+//car1.X = 5;
+//car1.Speed = 7;
+car1.X = 6;
+car1.Speed = 7;
 
+
+var car2 = new Car();
+car2.SetX(7);
+car2.SetSpeed(3);
+
+Console.WriteLine("Скорость машины №2 = {0}", car2.GetSpeed());
+//car2.X = 10;
+//car2.Speed = 3;
+
+
+Console.WriteLine("Программа завершена.");
+Console.ReadLine();
---
+++ IntroOOP/Transport/Car.cs
@@ -0,0 +1,67 @@
+﻿namespace IntroOOP.Transport;
+
+public class Car
+{
+    private static List<Car> __KnownCars = new List<Car>();
+
+    public static int GetKnownCarsCount()
+    {
+        return __KnownCars.Count;
+    }
+
+    private double _X;
+
+    private double _Speed;
+
+    public double GetX()
+    {
+        return _X;
+    }
+
+    public void SetX(double x)
+    {
+        _X = x;
+    }
+
+    public double ReadOnlyX => _X;
+
+    public double X
+    {
+        get
+        {
+            return _X;
+        }
+        set
+        {
+            _X = value;
+        }
+    }
+
+    public double GetSpeed()
+    {
+        return _Speed;
+    }
+
+    public void SetSpeed(double speed)
+    {
+        _Speed = speed;
+    }
+
+    public double Speed
+    {
+        get => _Speed;
+        set => _Speed = value;
+    }
+
+    //private string _Model;
+
+    //public string Model
+    //{
+    //    get => _Model;
+    //    set => _Model = value;
+    //}
+
+    public string Model { get; set; }
+
+
+}
