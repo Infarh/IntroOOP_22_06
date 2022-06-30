e9a958a4a30530a5a02d0f984eb8439eecafb02a
Infarh <shmachilin@yandex.ru>
2022-06-30 19:48:34 +0300

    Удаление хлама

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,44 +1,7 @@
 ﻿using IntroOOP;
 using IntroOOP.Transport;
 
-Car car1 = new Car("Лада Гранта", new Vector2D(-100, 0), new Vector2D());
-//car1.SetX(5);
-//car1.Speed = 7;
-//car1.X = 6;
-//car1.Speed = 7;
 
-
-var car2 = new Car("Грузовик", new Vector2D(30, 0), new Vector2D(10, 0));
-//car2.SetX(30);
-//car2.SetSpeed(10);
-
-const double T = 60;
-const double dt = 0.1;
-for (var t = 0.0; t < T; t += dt)
-{
-    car1.Move(dt);
-    car2.Move(dt);
-
-    car1.Acceleration = new Vector2D(
-        1 / (1 + Math.Exp(-(t - 5) / 2)),
-        1 / (1 + Math.Exp(-(t - 2) / 1.3)));
-
-    Console.WriteLine("t = {0:f1}", t);
-
-    car1.PrintPosition();
-    car2.PrintPosition();
-
-    Console.WriteLine();
-}
-
-var z1 = new Complex(5, 7);
-var z2 = new Complex(6, 2);
-
-var z3 = z1 + z2;
-
-//Console.WriteLine("Скорость машины №2 = {0}", car2.GetSpeed());
-//car2.X = 10;
-//car2.Speed = 3;
 
 
 Console.WriteLine("Программа завершена.");
