ecee14be001fb755468b593702562d2d99830a34
Infarh <shmachilin@yandex.ru>
2022-07-04 20:20:12 +0300

    Методы добавления в начало и конец списка

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,7 +1,18 @@
 ﻿
 using Utilities;
 
-var list = new RefList<int>();
+var list1 = new RefList<int>();
+
+list1.AddFirst(1);
+list1.AddFirst(2);
+list1.AddFirst(3);
+
+var list2 = new RefList<int>();
+
+list2.AddLast(3);
+list2.AddLast(2);
+list2.AddLast(1);
+
 //var list_node = new RefList<int>.Node(5);
 
 Console.WriteLine("Hello, World!");
--- Utilities/RefList.cs
+++ Utilities/RefList.cs
@@ -6,9 +6,9 @@ public class RefList<T>
     {
         public T Value { get; set; }
 
-        public Node? Prev { get; set; }
+        public Node? Prev { get; internal set; }
 
-        public Node? Next { get; set; }
+        public Node? Next { get; internal set; }
 
         internal Node(T value) => Value = value;
     }
@@ -19,8 +19,50 @@ public class RefList<T>
 
     public Node? Last { get; set; }
 
-    public void AddFirst(T value)
+    public Node AddFirst(T value)
     {
         var node = new Node(value);
+
+        _Count++;
+
+        //if (ReferenceEquals(First, null))
+        //if (Equals((object)First, null))
+        //if (First is not { })
+        if (First is null) // Если список был пуст
+        {
+            First = node;
+            Last = node;
+
+            return node;
+        }
+
+        // Если в списке что-то уже было, то надо изменить первый узел
+
+        node.Next = First;
+        First.Prev = node;
+        First = node;
+
+        return node;
+    }
+
+    public Node AddLast(T value)
+    {
+        var node = new Node(value);
+
+        _Count++;
+
+        if (Last is null)
+        {
+            Last = node;
+            First = node;
+
+            return node;
+        }
+
+        node.Prev = Last;
+        Last.Next = node;
+        Last = node;
+
+        return node;
     }
 }
