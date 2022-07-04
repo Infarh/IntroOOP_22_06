05ca1605165ff470e62f2f6ea31ff42c50f9c773
Infarh <shmachilin@yandex.ru>
2022-07-04 21:35:22 +0300

    Чтение данных из файла и их обработка без сохранения их в памяти

--- IntroOOP/ReadingDataTest.cs
+++ IntroOOP/ReadingDataTest.cs
@@ -5,7 +5,7 @@ namespace IntroOOP;
 
 public static class ReadingDataTest
 {
-    public static void Run()
+    public static void Run0()
     {
         const string data_file_path = @"Data\WeatherMoscow.csv";
 
@@ -50,5 +50,51 @@ public static class ReadingDataTest
             data.Add(info);
         }
 
+    }
+    public static void Run()
+    {
+        const string data_file_path = @"Data\WeatherMoscow.csv";
+
+        var data_file = new FileInfo(data_file_path);
+
+        var average_temp = EnumWeather(data_file).Average(weather => weather.Temperature);
+    }
+
+    private static IEnumerable<WeatherInfo> EnumWeather(FileInfo file)
+    {
+        var lines_count = 0;
+        foreach (var line in file.EnumLines())
+        {
+            lines_count++;
+            if (lines_count < 8) continue;
+
+            var values = line.Split(';');
+
+            //var date = DateTime.Parse(values[0].Trim('"'));
+            if (!DateTime.TryParse(values[0].Trim('"'), out var date))
+                continue;
+
+            //var temperature = double.Parse(values[1].Trim('"'), CultureInfo.InvariantCulture);
+            if (!double.TryParse(values[1].Trim('"'), NumberStyles.Any, CultureInfo.InvariantCulture, out var temperature))
+                continue;
+
+            //var pressure = double.Parse(values[2].Trim('"'), CultureInfo.InvariantCulture);
+            if (!double.TryParse(values[2].Trim('"'), NumberStyles.Any, CultureInfo.InvariantCulture, out var pressure))
+                continue;
+
+            //var info = new WeatherInfo();
+            //info.Time = date;
+            //info.Temperature = temperature;
+            //info.Pressure = pressure;
+
+            var info = new WeatherInfo
+            {
+                Time = date,
+                Temperature = temperature,
+                Pressure = pressure,
+            };
+
+            yield return info;
+        }
     }
 }
