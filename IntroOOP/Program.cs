0c490d3a5d731342c9d16648fa9b567724402e1b
Infarh <shmachilin@yandex.ru>
2022-07-04 20:09:14 +0300

    Заготовка списка

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,3 +1,7 @@
 ﻿
+using Utilities;
+
+var list = new RefList<int>();
+//var list_node = new RefList<int>.Node(5);
 
 Console.WriteLine("Hello, World!");
---
+++ Utilities/RefList.cs
@@ -0,0 +1,26 @@
+﻿namespace Utilities;
+
+public class RefList<T>
+{
+    public class Node
+    {
+        public T Value { get; set; }
+
+        public Node? Prev { get; set; }
+
+        public Node? Next { get; set; }
+
+        internal Node(T value) => Value = value;
+    }
+
+    private int _Count;
+
+    public Node? First { get; set; }
+
+    public Node? Last { get; set; }
+
+    public void AddFirst(T value)
+    {
+        var node = new Node(value);
+    }
+}
