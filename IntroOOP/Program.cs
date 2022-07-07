ddc8ba60780570ae7f0be0dbd92da9c44ecf2b2c
Infarh <shmachilin@yandex.ru>
2022-07-07 20:25:17 +0300

    Обзор вариантов сериализации данных

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,4 +1,7 @@
-﻿using IntroOOP;
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+using IntroOOP;
 using IntroOOP.Students;
 
 const string names_file_name = @"Data\Names.txt";
@@ -8,7 +11,10 @@ var (last_names, first_names, patronymics) = data_file.GetNames();
 
 var students = Student.CreateRandomStudents(last_names, first_names, patronymics);
 
-
+//var serializer_xml = new XmlSerializer(typeof(Student[]));
+//var serializer_bin = new BinaryFormatter();
+//var serializer_json = new JsonSerializer(); // используется иначе
+//JsonSerializer.
 
 Console.WriteLine("Конец...");
 Console.ReadLine();
