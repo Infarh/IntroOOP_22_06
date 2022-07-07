4a6c350a22a5e06d8840279ade910af6b373ed54
Infarh <shmachilin@yandex.ru>
2022-07-07 20:08:44 +0300

    Рефакторинг - убираем логику генерации стулайных студентов в класс Student

--- IntroOOP/NamesServcie.cs
+++ IntroOOP/NamesServcie.cs
@@ -16,4 +16,20 @@ public static class NamesService
             yield return (values[0], values[1], values[2]);
         }
     }
+
+    public static (string[] LastNames, string[] FirstNames, string[] Patronymics) GetNames(this FileInfo file)
+    {
+        var last_names = new List<string>();
+        var first_names = new List<string>();
+        var patronymics = new List<string>();
+
+        foreach (var values in file.EnumPersonNames())
+        {
+            last_names.Add(values.LastName);
+            first_names.Add(values.FirstName);
+            patronymics.Add(values.Patronymic);
+        }
+
+        return (last_names.ToArray(), first_names.ToArray(), patronymics.ToArray());
+    }
 }
--- IntroOOP/Students/Student.cs
+++ IntroOOP/Students/Student.cs
@@ -4,6 +4,24 @@ namespace IntroOOP.Students;
 
 public class Student : NamedItem
 {
+    public static Student[] CreateRandomStudents(string[] LastNames, string[] FirstNames, string[] Patronymics, int Count = 1000)
+    {
+        var students = new Student[Count];
+        var rnd = new Random(Environment.TickCount);
+        for (var i = 0; i < Count; i++)
+        {
+            students[i] = new Student
+            {
+                Id = i + 1,
+                LastName = LastNames[rnd.Next(LastNames.Length)],
+                Name = FirstNames[rnd.Next(FirstNames.Length)],
+                Patronymic = Patronymics[rnd.Next(Patronymics.Length)],
+            };
+        }
+
+        return students;
+    }
+
     public string LastName { get; set; }
 
     public string Patronymic { get; set; }
