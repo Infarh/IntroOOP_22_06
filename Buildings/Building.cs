47c9e3c29b62ad39a2dbe52d4f7c9f26ca413db6
Infarh <shmachilin@yandex.ru>
2022-07-11 19:44:40 +0300

    Здание

---
+++ Buildings/Building.cs
@@ -0,0 +1,34 @@
+﻿namespace Buildings;
+
+
+public class Building
+{
+    private readonly int _Id;
+
+    private readonly int _FloorsCount;
+
+    private readonly int _FlatsPerFloorCount;
+
+    private readonly double _FloorHeight;
+
+    private readonly int _EntrancesCount;
+
+    public int Id => _Id;
+
+    public int FloorsCount => _FloorsCount;
+
+    public int FlatsPerFloorCount => _FlatsPerFloorCount;
+
+    public double FloorHeight => _FloorHeight;
+
+    public int EntrancesCount => _EntrancesCount;
+
+    public Building(int Id, int FloorsCount, int FlatsPerFloorCount, double FloorHeight, int EntrancesCount)
+    {
+        _Id = Id;
+        _FloorsCount = FloorsCount;
+        _FlatsPerFloorCount = FlatsPerFloorCount;
+        _FloorHeight = FloorHeight;
+        _EntrancesCount = EntrancesCount;
+    }
+}
--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿
 
+
 Console.WriteLine("Конец...");
 Console.ReadLine();
