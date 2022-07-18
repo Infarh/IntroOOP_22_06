b25cb88451c51ad53d03dc4757ac918ae54596bd
Infarh <shmachilin@yandex.ru>
2022-07-18 19:25:54 +0300

    Разница между EnumerateFiles и GetFiles

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -4,11 +4,26 @@
 //file.Attributes |= FileAttributes.Archive;
 //file.Attributes &= ~FileAttributes.Archive;
 
-//var dir = new DirectoryInfo("c:\\123");
+var dir = new DirectoryInfo("c:\\123");
+//dir.EnumerateFiles()
+//dir.GetFiles();
 //var drives = DriveInfo.GetDrives();
 
 //File.Create()
 //Directory.EnumerateFiles("c:\\123", "*.*").ToArray();
+
+//Directory.GetFiles("c:\\123", "*.*")
+//Directory.EnumerateFiles("c:\\123", "*.*")
+
+//if (Directory.GetFiles("c:\\123", "*.*").Length > 0)
+//{
+
+//}
+
+//if (Directory.EnumerateFiles("c:\\123", "*.*").Any())
+//{
+
+//}
 
 Console.WriteLine("End.");
 Console.ReadLine();
