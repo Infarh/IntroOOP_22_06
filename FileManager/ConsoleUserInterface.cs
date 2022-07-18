552edf02d1fe5333b159f17290d2db29d3939099
Infarh <shmachilin@yandex.ru>
2022-07-18 19:50:29 +0300

    Ввод чисел с консоли

--- FileManager/ConsoleUserInterface.cs
+++ FileManager/ConsoleUserInterface.cs
@@ -12,7 +12,7 @@ public class ConsoleUserInterface : IUserInterface
         Console.WriteLine(str);
     }
 
-    public string ReadLine(string? Prompt, bool PromptNewLine = true)
+    private void WritePrompt(string? Prompt, bool PromptNewLine)
     {
         //if(Prompt != null && Prompt.Length > 0)
         if (Prompt is { Length: > 0 })
@@ -20,7 +20,48 @@ public class ConsoleUserInterface : IUserInterface
                 WriteLine(Prompt);
             else
                 Write(Prompt);
+    }
+
+    public string ReadLine(string? Prompt, bool PromptNewLine = true)
+    {
+        WritePrompt(Prompt, PromptNewLine);
 
         return Console.ReadLine()!;
+    }
+
+    public int ReadInt(string? Prompt, bool PromptNewLine = true)
+    {
+        bool success;
+        int value;
+        do
+        {
+            WritePrompt(Prompt, PromptNewLine);
+
+            var input = Console.ReadLine();
+            success = int.TryParse(input, out value);
+            if(!success)
+                WriteLine("Строка имела неверный формат");
+        }
+        while (!success);
+
+        return value;
+    }
+
+    public double ReadDouble(string? Prompt, bool PromptNewLine = true)
+    {
+        bool success;
+        double value;
+        do
+        {
+            WritePrompt(Prompt, PromptNewLine);
+
+            var input = Console.ReadLine();
+            success = double.TryParse(input, out value);
+            if (!success)
+                WriteLine("Строка имела неверный формат");
+        }
+        while (!success);
+
+        return value;
     }
 }
--- FileManager/IUserInterface.cs
+++ FileManager/IUserInterface.cs
@@ -7,4 +7,8 @@ public interface IUserInterface
     void WriteLine(string str);
 
     string ReadLine(string? Prompt, bool PromptNewLine = true);
+
+    int ReadInt(string? Prompt, bool PromptNewLine = true);
+
+    double ReadDouble(string? Prompt, bool PromptNewLine = true);
 }
